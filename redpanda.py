a0b41be2ecc6384fe9a88bef2fe2947accd05599
Nabilla Rahmani <nabilla.rahmani@tokopedia.com>
2017-09-13 10:20:33 +0700

    init flask-2

--- redpanda.py
+++ redpanda.py
@@ -4,9 +4,6 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask_dotenv import DotEnv
-from flask_mail import Mail
-from flask_mail import Message
-from form import FeedbackForm
 
 def create_app():
 	env = DotEnv()
@@ -24,30 +21,9 @@ def create_app():
 		"""
 			Render the view of index page.
 		"""
-		form = FeedbackForm()
-		return render_template('index.html', title='Tri Sinergi Mitra', current_page='TSM', form=form)
 
-	@app.route("/send_feedback", methods=['POST'])
-	def send_feedback():
-		"""
-			Do action with python form
-		"""
-		if request.method == 'POST' :
-			#result = request.form
-			result = request.form['email']
-			msg_subject = "You got an email from {} / {} - {}".format(request.form['email'], request.form['full_name'], request.form['subject'])
-			msg_body = request.form['body']
-			send_email(msg_subject,'trisinergim@gmail.com', ['trisinergim@gmail.com'], msg_body)
-			return redirect(url_for('index'))
-		else:
-			flash('The message has not been sent due to unknown failure')
-			return redirect(url_for('index'))
+		return render_template('index.html', title='RedPanda', current_page='RedPanda', form=form)
 
-	def send_email(subject, sender, recipients, text_body):
-		msg = Message(subject, sender=sender, recipients=recipients)
-		msg.body = text_body
-		mail.send(msg)
-		return
 
 	@app.errorhandler(404)
 	def page_not_found_error(err):
